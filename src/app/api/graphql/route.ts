0c6b69f8c32976ccd7711319261b2d415f90884d
Aloïs GUITTON <aloisguitton@orange.fr>
2023-10-18 19:41:08 +0200

    feature(prisma-graphql): implement graphql and serve it with graphql

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,16 +1,51 @@
 import { ApolloServer } from "@apollo/server";
 import { NextRequest } from "next/server";
+import prisma from "@/app/lib/prisma";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
 const typeDefs = `#graphql
+  type Todo {
+    id: Int!
+    title: String!
+    done: Boolean!
+    createdAt: String!
+  }
+
   type Query {
-    Hello: String
+    todos: [Todo!]!
+  }
+
+  type Mutation {
+    createTodo(title: String!): Todo!
+    updateTodo(id: Int!, done: Boolean!): Todo!
   }
 `;
 
 const resolvers = {
+  Mutation: {
+    createTodo: async (_: never, args: { title: string }) => {
+      const newTodo = prisma.todo.create({
+        data: {
+          title: args.title,
+          done: false,
+        },
+      });
+      return newTodo;
+    },
+    updateTodo: async (_: never, args: { done: boolean; id: number }) => {
+      const updatedTodo = await prisma.todo.update({
+        where: {
+          id: args.id,
+        },
+        data: {
+          done: args.done,
+        },
+      });
+      return updatedTodo;
+    },
+  },
   Query: {
-    Hello: () => "Word!",
+    todos: async () => prisma.todo.findMany(),
   },
 };
 
